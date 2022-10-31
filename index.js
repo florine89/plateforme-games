43a3ce59a0fb7322a8ed5fb71e8955a9a8317f93
florine89 <ferreiraflorine@gmail.com>
2022-10-31 10:51:01 +0100

    etape 2 ok

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //on require express ici
 const express = require('express')
 const app = express()
-
+// modules personnels
+const routes = require('./routes')
 // on précise le moteur de rendu via EJS
 app.set('view engine', 'ejs');
 //on indique que les views d'EJS sont dans le dossier views
@@ -9,10 +10,9 @@ app.set('views', 'views');
 //On rends statiques les fichiers du dossier public
 app.use(express.static(__dirname+'/public'))
 
+//On précise à express d'utiliser le fichier route opour gérer les adresses
+app.use(routes);
 
 
-app.get('/', function (request, response) {
-  response.send('Hello World')
-})
 
 app.listen((3000), () => console.log('Server listening  on  port 3000'));
---
+++ routes.js
@@ -0,0 +1,9 @@
+//on require express ici
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (request, response) => {
+    response.render('index')
+  })
+
+module.exports= router;
